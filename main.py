ac20c3d29827e2eb83209698847b9e91169ab55f
Aayush Mishra <88718138+iamAayushMishra@users.noreply.github.com>
2021-11-30 10:36:58 +0000

    Added procfile and req.txt

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World")
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = os.environ['TOKEN']
+
+bot = commands.Bot(command_prefix="f!")
+
+@bot.event
+async def on_ready():
+  print(f"{bot.user.name} has been connected")
+
+@bot.command()
+async def ping(ctx):
+  await ctx.send(f"Pong! in `{round(bot.latency * 1000)}`")
+
+
+
+@bot.run(TOKEN)
